66d7c08ae901a5ef90c2dc66be45f59da11cad51
dfontana <dyfontana@gmail.com>
2017-10-13 22:18:23 -0400

    Updated files. Error in update.

--- update_links.sh
+++ update_links.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-cd ~/dotfiles
+cd ~/dotfiles || exit
 for item in .*; do
-    if [ $item != "." ] && [ $item != ".." ]; then
-        if ln -s ~/dotfiles/$item ~/$item &> /dev/null; then
-            echo "[$item] Linked."
+    if [ "$item" != "." ] && [ "$item" != ".." ] && [ "$item" != ".git" ]; then
+        if [ "$item" = ".zsh_custom" ]; then
+            ln -s ~/dotfiles/$item ~/.oh-my-zsh/custom &> /dev/null
+        else
+            ln -s ~/dotfiles/$item ~/$item &> /dev/null
+        fi
+
+        LINKED=$?
+
+        if [ $LINKED -eq 0 ]; then
+             echo "[$item] Linked."
         else
             echo "[$item] Exists."
         fi
     fi
-
-
-
 done
