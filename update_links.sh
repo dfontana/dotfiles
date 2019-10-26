ab70b9fa2aa21c7d9f6739ec8e7b07964bc798f3
Dylan Fontana <dyfontana@gmail.com>
2019-10-26 16:22:50 -0400

    Updating zsh, tmux

--- update_links.sh
+++ update_links.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-cd ~/dotfiles || exit
 for item in .*; do
     if [ "$item" != "." ] && [ "$item" != ".." ] && [ "$item" != ".git" ]; then
         if [ "$item" = ".zsh_custom" ]; then
-            ln -ns ~/dotfiles/$item ~/.oh-my-zsh/custom &> /dev/null
+            ln -ns ./$item ~/.oh-my-zsh/custom &> /dev/null
         else
-            ln -ns ~/dotfiles/$item ~/$item &> /dev/null
+            ln -ns ./$item ~/$item &> /dev/null
         fi
 
         LINKED=$?
