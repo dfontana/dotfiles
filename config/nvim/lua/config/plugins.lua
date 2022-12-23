88f50310f88d6c77d713a34523db059ddd84ba8c
Dylan Fontana <dyfontana@gmail.com>
2022-12-23 02:18:06 -0500

    Added bigfile, for big files

--- config/nvim/lua/config/plugins.lua
+++ config/nvim/lua/config/plugins.lua
@@ -1,9 +1,10 @@
 return {
+	{ "LunarVim/bigfile.nvim", lazy = false },
 	{ "nvim-lua/plenary.nvim", lazy = false },
 	{ "nvim-lua/popup.nvim", lazy = false },
 	{ "moll/vim-bbye", lazy = false },
 	{ "kevinhwang91/nvim-bqf", lazy = false },
 	{ "mattn/webapi-vim", lazy = false },
 	{ "rafamadriz/friendly-snippets", lazy = false },
-	{ "folke/which-key.nvim", lazy = false },	
+	{ "folke/which-key.nvim", lazy = false },
 }
