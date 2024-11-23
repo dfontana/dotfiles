95d5d44508c48cad040430e1e7069668ab2b8e16
Dylan Fontana <dyfontana@gmail.com>
2024-11-23 11:07:38 -0500

    Ditch Nvim,Lvim,Doom

--- config/nvim/lua/config/plugins.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{ "LunarVim/bigfile.nvim", lazy = false },
-	{ "nvim-lua/plenary.nvim", lazy = false },
-	{ "nvim-lua/popup.nvim", lazy = false },
-	{ "moll/vim-bbye", lazy = false },
-	{ "kevinhwang91/nvim-bqf", lazy = false },
-	{ "mattn/webapi-vim", lazy = false },
-	{ "folke/which-key.nvim", lazy = false },
-}
